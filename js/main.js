4dd24deb58ee1865dca5489598778a54677e9ac9
Maksim Aisin <39880369+TavaniX@users.noreply.github.com>
2020-12-17 20:11:09 +0300

    Delete main.js

--- js/main.js
+++
@@ -1,6 +0,0 @@
-const menuBtn = document.querySelector('.m-menu');
-const menu = document.querySelector(".menu-sidebar")
-
-menuBtn.addEventListener('click', ()=> {
-    menu.classList.toggle("active");
-})
